11aee307ebafd7f48dd8f624addebfd2162e2e8c
Gidonyx <bloodraven98@gmail.com>
2018-11-20 00:37:46 +0500

    add two files клиент и сервер

---
+++ Client.py
@@ -0,0 +1,55 @@
+import socket,time
+
+client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_sock.connect(('127.0.0.1', 53210))
+
+server = ('127.0.0.1', 53210)
+
+chanel = 0
+
+#Выбираем канал
+while chanel != '/set_chanelOne' and chanel != '/set_chanelTwo':
+    print("Введите команду для выбора канала /set_chanelOne или /set_chanelTwo")
+    chanel = input()
+client_sock.sendall(chanel.encode())
+
+#Вводим имя
+print('Введите имя')
+name = input()
+client_sock.sendall(name.encode())
+
+while True:
+
+    #Читаем логи
+    history = open('text.txt', 'r')
+    chtenie = history.read()
+    print(chtenie)
+    history.close()
+
+    #Проверяем на безсимвольность
+    print("Введите что-нибудь")
+    text = input()
+    if len(text) == 0:
+        print("В тексте нет ни одного символа")
+        continue
+
+    #Запоминаем время и записываем в лог
+    vremya_client = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    history = open('text.txt', 'a')
+    history.write(vremya_client + ' ' + '|' + ' ' + name + ':' + ' ' + text + '\n')
+    history.close()
+
+    #Отправляем сообщение
+    client_sock.sendall(text.encode())
+    data = client_sock.recv(1024)
+    print('Received', repr(data))
+
+    #Как закрыть клиент
+    if text == 'Exit':
+        client_sock.close()
+        break
+
+
+
+
+#client_sock.sendall(b'%(clienttext)s'%{"clienttext": text})
---
+++ Server.py
@@ -0,0 +1,58 @@
+import socket, time
+
+serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
+serv_sock.bind(('127.0.0.1', 53210))
+serv_sock.listen(10)
+
+chanelOne = 0
+chanelTwo = 0
+
+clients = []
+clients2 = []
+
+serverlog = open('serverlog.txt', 'r')
+chtenie = serverlog.read()
+print(chtenie)
+serverlog.close()
+
+while True:
+    client_sock, client_addr = serv_sock.accept()
+    print('Соединено ', client_addr)
+
+    while True:
+        vremya = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+        data = client_sock.recv(1024)
+
+        serverlog = open('serverlog.txt', 'a')
+        serverlog.write(vremya + ' ' + '|' + ' ' + str(data) + '\n')
+        serverlog.close()
+
+       # if data == bytes('/set_nickname'.encode()):
+
+
+        if data == bytes('/set_chanelOne'.encode()):
+            clients.append(client_addr)
+            print('В первый канал вошел новый клиент', client_addr, '\n', 'Действующие клиенты', ' ', clients)
+        if data == bytes('/set_chanelTwo'.encode()):
+            clients2.append(client_addr)
+            print('Во второй канал вошел новый клиент', client_addr, '\n', 'Действующие клиенты', ' ', clients2)
+
+
+        print(vremya, "|", client_addr, "-", data)
+
+
+        if not data:
+            break
+
+        if client_addr in clients:
+            for i in range(len(clients)):
+                client_sock.sendall(data)
+
+        if client_addr in clients2:
+            for i in range(len(clients2)):
+                client_sock.sendall(data)
+
+        #client_sock.sendall(data)
+
+    client_sock.close()
